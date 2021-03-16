6581425c6ae80d4e83e4a8a49cb67e1230c879dc
Sebastian Cardona Loaiza <cardonaloaizasebastian112@gmail.com>
2021-03-15 22:59:37 -0500

    try fix cors

--- api/config/config.js
+++ api/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'secret': '*987654321bellarose-inventory_billing123456789*',
-  'front_dev': 'https://bellarose-web-qa.wl.r.appspot.com/',
+  'front_dev': 'https://bellarose-web-qa.wl.r.appspot.com',
   'keyPath': './api/config/key2/',
   'key_dev': 'bellarose-qa-8a591141e80f.json',
   'version': '1.0.0',
